c2e46620cbcbd73748b569f1b2f732163e97446c
bimapriaaditya <bpabima@gmail.com>
2025-07-08 21:55:21 +0700

    fix unused import

--- src/pages/Abouts/AboutScreen.tsx
+++ src/pages/Abouts/AboutScreen.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function AboutScreen() {
   return (
     <div>AboutScreen</div>
--- src/pages/Blogs/BlogScreen.tsx
+++ src/pages/Blogs/BlogScreen.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function BlogScreen() {
   return (
     <div>BlogScreen</div>
